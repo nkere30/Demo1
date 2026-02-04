c1e10c543e0fe9755b58495ca8649fa116864cfa
nkere30 <ninokereselidze30@gmail.com>
2026-02-05 01:02:51 +0400

    Enabled Lambda URL for task02

--- task02/japp/src/main/java/com/task02/HelloWorld.java
+++ task02/japp/src/main/java/com/task02/HelloWorld.java
@@ -7,7 +7,6 @@ import com.syndicate.deployment.model.RetentionSetting;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,9 +24,7 @@ import java.util.Map;
 public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 
     @Override
-	public Map<String, Object> handleRequest(Object request, Context context) {
-
-        Map<String, Object> response = new HashMap<>();
+    public Map<String, Object> handleRequest(Object request, Context context) {
 
         Map<String, Object> event = (Map<String, Object>) request;
         String path = (String) event.get("rawPath");
@@ -36,15 +33,23 @@ public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
         Map<String, Object> http = (Map<String, Object>) requestContext.get("http");
         String method = (String) http.get("method");
 
+        Map<String, Object> response = new HashMap<>();
+
         if ("/hello".equals(path) && "GET".equals(method)) {
             response.put("statusCode", 200);
-            response.put("message", "Hello from Lambda");
+            response.put(
+                    "body",
+                    "{\"statusCode\":200,\"message\":\"Hello from Lambda\"}"
+            );
         } else {
             response.put("statusCode", 400);
             response.put(
-                    "message",
-                    "Bad request syntax or unsupported method. Request path: " + path + ". HTTP method: " + method);
+                    "body",
+                    "{\"statusCode\":400,\"message\":\"Bad request syntax or unsupported method. Request path: "
+                            + path + ". HTTP method: " + method + "\"}"
+            );
         }
+
         return response;
-	}
+    }
 }
