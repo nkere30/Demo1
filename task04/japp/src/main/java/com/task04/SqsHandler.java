e444e782edf72b9b181196739e09314b675cd6de
nkere30 <ninokereselidze30@gmail.com>
2026-03-02 12:43:46 +0400

    task04 sqs and sns with lambda

---
+++ task04/japp/src/main/java/com/task04/SqsHandler.java
@@ -0,0 +1,34 @@
+package com.task04;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@LambdaHandler(
+        lambdaName = "sqs_handler",
+        roleName = "sqs_handler-role",
+        isPublishVersion = true,
+        aliasName = "${lambdas_alias_name}",
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+
+@SqsTriggerEventSource(
+        targetQueue = "async_queue",
+        batchSize = 10
+)
+public class SqsHandler implements RequestHandler<Object, Map<String, Object>> {
+
+    public Map<String, Object> handleRequest(Object request, Context context) {
+        System.out.println("Event: " + request);
+
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("statusCode", 200);
+        resultMap.put("body", "Hello from Lambda");
+        return resultMap;
+    }
+}
