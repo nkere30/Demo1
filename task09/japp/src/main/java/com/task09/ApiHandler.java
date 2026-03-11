3c779f977999435897f191ba51e1de029242bc54
nkere30 <ninokereselidze30@gmail.com>
2026-03-11 07:32:24 +0400

    Fix no response body in postman

--- task09/japp/src/main/java/com/task09/ApiHandler.java
+++ task09/japp/src/main/java/com/task09/ApiHandler.java
@@ -2,7 +2,6 @@ package com.task09;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaLayer;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
@@ -47,17 +46,18 @@ public class ApiHandler implements RequestHandler<Object, Map<String, Object>> {
             if ("/weather".equals(path) && "GET".equals(method)) {
                 OpenMeteoClient client = new OpenMeteoClient();
                 String weather = client.getWeather();
-                ObjectMapper mapper = new ObjectMapper();
-                Map<String, Object> weatherMap = mapper.readValue(weather, Map.class);
                 resultMap.put("statusCode", 200);
-                resultMap.putAll(weatherMap);
+                resultMap.put("headers", Map.of("Content-Type", "application/json"));
+                resultMap.put("body", weather);
             } else {
                 resultMap.put("statusCode", 400);
-                resultMap.put("message", "Bad request syntax or unsupported method. Request path: " + path + ". HTTP method: " + method);
+                resultMap.put("headers", Map.of("Content-Type", "application/json"));
+                resultMap.put("body", "{\"statusCode\":400,\"message\":\"Bad request syntax or unsupported method. Request path: " + path + ". HTTP method: " + method + "\"}");
             }
         } catch (Exception e) {
             resultMap.put("statusCode", 400);
-            resultMap.put("message", "Bad request syntax or unsupported method.");
+            resultMap.put("headers", Map.of("Content-Type", "application/json"));
+            resultMap.put("body", "{\"statusCode\":400,\"message\":\"" + e.getMessage() + "\"}");
         }
         return resultMap;
     }
